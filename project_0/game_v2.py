f1dfa40f6fd45ec04d82c66998b9a2f8da4958ef
albone-222 <79514164692@mail.ru>
2022-04-08 00:15:49 +0300

    PEP8 format

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,12 +1,14 @@
 """Игра угадай число
 компьютер сам загадывает и сам отгадывает число, количество
-попыток не должно превышать 20"""
+попыток не должно превышать 20.  
+"""
+
 import numpy as np
-import math
+
 
 def random_predict(number:int=1) -> int:
     """Рандомно угадываем число из диапазона, с корректировкой
-    диапазона после каждой итерации
+    диапазона после каждой итерации.
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -18,11 +20,12 @@ def random_predict(number:int=1) -> int:
     count = 0
     range_min = 1
     range_max = 101
+    
     while True:
         count += 1
-        predict_number = np.random.randint(range_min, range_max) # предполагаемое число
+        predict_number = np.random.randint(range_min, range_max)  # предполагаемое число
         if number == predict_number:
-            break # выход из цикла, если угадали
+            break  # выход из цикла, если угадали
         elif number > predict_number:
             range_min = predict_number + 1
         else:
@@ -31,8 +34,9 @@ def random_predict(number:int=1) -> int:
 
 
 def middle_predict(number:int=1) -> int:
-    """Методично в качестве угадываемого числа берем середину диапазона, 
-    с каждой попыткой уменьшая диапазон в геометрической прогрессии 
+    """Методично в качестве угадываемого числа берем середину
+    диапазона (целую часть), с каждой попыткой уменьшая диапазон 
+    в геометрической прогрессии. 
     
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
@@ -44,11 +48,12 @@ def middle_predict(number:int=1) -> int:
     count = 0
     range_min = 1
     range_max = 101
+    
     while True:
         count += 1
-        predict_number = int((range_min+range_max) * 0.5) # предполагаемое число
+        predict_number = int((range_min+range_max) * 0.5)  # предполагаемое число
         if number == predict_number:
-            break # выход из цикла, если угадали
+            break  # выход из цикла, если угадали
         elif number > predict_number:
             range_min = predict_number + 1
         else:
@@ -56,27 +61,23 @@ def middle_predict(number:int=1) -> int:
     return(count)
 
 
-
-
-
 def score_game(func_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
 
     Args:
-        random_predict ([type]): функция угадывания
+        func_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
 
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    count_ls = []  # список для сохранения количества попыток
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(func_predict(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls))  # находим среднее количество попыток
 
     print(f'Алгоритм {func_predict.__name__} угадывает число в среднем за: {score} попыток')
     return(score)
